3d583630c559b66f445917956bb27fbf5ef97d2e
RobertFC15 <robert02111102@gmail.com>
2024-10-21 21:59:21 -0300

    Add files via upload

---
+++ Atividade3.java
@@ -0,0 +1,25 @@
+/*Robert Franklin C. Coelho
+ * Desenvolvimento de Aplicações
+ * Turma C
+ */
+
+
+public class Atividade3{
+    public static void main(String[] args) throws Exception {
+
+        for(int i = 1; i <= 1000; i++) {
+
+            if (i % 11 == 5) {
+                System.out.println(i);
+                
+            }
+                
+            }
+
+            
+
+        }
+    }
+        
+ 
+
